9d1232563bf5ccbdf961748141b4d84356db02f7
aiibe <souksavanh.sy@gmail.com>
2021-10-27 16:28:09 +0200

    Populate home page

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,8 +1,27 @@
 import { PureComponent } from "react";
+import { Link } from "react-router-dom";
+
+import { findAll } from "./utils/query";
 
 class Home extends PureComponent {
+  state = {
+    articles: [],
+  };
+
+  componentDidMount() {
+    this.setState({ articles: findAll() });
+  }
+
   render() {
-    return <p>Home</p>;
+    return (
+      <ul>
+        {this.state.articles.map((a) => (
+          <li>
+            <Link to={`/${a.id}`}>{a.title}</Link>
+          </li>
+        ))}
+      </ul>
+    );
   }
 }
 
--- src/utils/query.js
+++ src/utils/query.js
@@ -1,5 +1,9 @@
 import data from "../data.json";
 
+export function findAll() {
+  return data;
+}
+
 export function findById(id) {
   return data.filter((i) => i.id === id);
 }
