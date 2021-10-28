5ad59c15a3394d21ed69a2bcc39f7bbe1a79391a
aiibe <souksavanh.sy@gmail.com>
2021-10-28 19:11:31 +0200

    Add css transition collapse

--- src/components/Collapse.jsx
+++ src/components/Collapse.jsx
@@ -16,7 +16,13 @@ class Collapse extends PureComponent {
       <div className="collapse">
         <div className="collapse__head" onClick={this.toggleContent.bind(this)}>
           <h2 className="collapse__title">{title}</h2>
-          <span className="collapse__toggle">
+          <span
+            className={
+              this.state.expanded
+                ? "collapse__toggle collapse__toggle--expand"
+                : "collapse__toggle"
+            }
+          >
             <svg viewBox="0 0 16 10">
               <path
                 d="M1.97011 0.992188L0.611694 2.36142L8.20963 9.96911L15.8076 2.35373L14.4492 0.992188L8.20963 7.24604L1.97011 0.992188Z"
