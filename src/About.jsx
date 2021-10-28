28ce6969313191049709a6cab27904605baece21
aiibe <souksavanh.sy@gmail.com>
2021-10-28 11:01:18 +0200

    Add top banner about page

--- src/About.jsx
+++ src/About.jsx
@@ -1,8 +1,19 @@
 import { PureComponent } from "react";
+import "./css/About.css";
+import about from "./about.png";
 
 class About extends PureComponent {
   render() {
-    return <p>A Propos</p>;
+    return (
+      <>
+        <div className="feature">
+          <div className="feature__image">
+            <img src={about} alt="mountain view" />
+          </div>
+        </div>
+        <div className="about"></div>
+      </>
+    );
   }
 }
 
