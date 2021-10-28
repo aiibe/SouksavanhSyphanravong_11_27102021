2c1d1807e63426e2bf6a4c0771fc21866a57eb88
aiibe <souksavanh.sy@gmail.com>
2021-10-28 10:38:14 +0200

    Style not found page

--- src/NotFound.jsx
+++ src/NotFound.jsx
@@ -6,9 +6,13 @@ class NotFound extends PureComponent {
   render() {
     return (
       <div className="container notfound">
-        <h1>404</h1>
-        <p>Oups! La page que vous demandez n'existe pas.</p>
-        <a href="/">Retourner sur la page d’accueil</a>
+        <div className="notfound__message">
+          <h1>404</h1>
+          <p>Oups! La page que vous demandez n'existe pas.</p>
+        </div>
+        <a className="notfound__redirect" href="/">
+          Retourner sur la page d’accueil
+        </a>
       </div>
     );
   }
