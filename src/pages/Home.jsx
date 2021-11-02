c7e7e0e9fde8d78149ca8ba16ac057f40cd6d2c1
aiibe <souksavanh.sy@gmail.com>
2021-11-02 10:11:24 +0100

    Fix shallow copy articles in Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,7 +10,7 @@ class Home extends PureComponent {
   };
 
   componentDidMount() {
-    this.setState({ articles: findAll() });
+    this.setState({ articles: [...findAll()] });
   }
 
   render() {
